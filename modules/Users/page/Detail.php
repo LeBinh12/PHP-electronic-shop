c16e8afde363bdcc86e21acb4713468568c69446
MAICHIVINH <vinh23861@gmail.com>
2025-06-26 09:47:15 +0700

    [update detail]

--- modules/Users/page/Detail.php
+++ modules/Users/page/Detail.php
@@ -59,7 +59,7 @@ $imageByProductId = $imageController->getImageById($id_product);
     <!-- Sp tương tự -->
     <div class="mt-5">
         <h4>Sản phẩm tương tự</h4>
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="row row-cols-1 row-cols-md-5 g-4">
             <?php
             $count = 0;
             foreach ($productByCategoryId as $item) {
@@ -71,7 +71,7 @@ $imageByProductId = $imageController->getImageById($id_product);
                         <img src="<?= $item['image_url'] ?>" class="card-img-top" alt="Sản phẩm 3">
                         <div class="card-body">
                             <h5 class="card-title"><?= $item['name'] ?></h5>
-                            <p class="text-danger fw-bold">Giá: <?= $item['price'] ?>đ</p>
+                            <p class="card-price">Giá: <?= $item['price'] ?>đ</p>
                         </div>
                     </div>
                 </div>
@@ -83,7 +83,7 @@ $imageByProductId = $imageController->getImageById($id_product);
     </div>
 
     <!-- Bài viết -->
-    <div class="mt-5">
+    <!-- <div class="mt-5">
         <h4>Bài viết liên quan</h4>
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
@@ -123,5 +123,5 @@ $imageByProductId = $imageController->getImageById($id_product);
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
