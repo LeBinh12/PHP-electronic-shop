9772c3a9d2569c619e703e5a3308b5b25114c4ca
MAICHIVINH <vinh23861@gmail.com>
2025-06-28 08:43:44 +0700

    [update supplier]

--- modules/Users/Layout/Supplier.php
+++ modules/Users/Layout/Supplier.php
@@ -2,29 +2,23 @@
 $supplierGetAll = $supplier->getAll();
 ?>
 
-<div class="container my-5">
-    <h2 class="text-center mb-4">Đối tác cung cấp</h2>
-
-    <div class="row g-3 supplier-grid">
+<div class="container py-5">
+    <h2 class="text-center mb-4 fw-bold text-uppercase">Đối tác cung cấp</h2>
+    <div class="row g-4">
         <?php foreach ($supplierGetAll as $sp) { ?>
             <div class="col-6 col-sm-4 col-md-3 col-lg-2">
-
-                <a href="index.php?supplier=<?= $sp['id'] ?>"
-                    class="text-decoration-none">
-
-                    <div class="card supplier-card h-100 text-center text-white">
-                        <img src="<?= htmlspecialchars($sp['image_url']) ?>"
-                            class="card-img-top img-fluid"
-                            alt="<?= htmlspecialchars($sp['name']) ?>">
-
-                        <div class="card-body p-2">
-                            <h6 class="card-title mb-0 fw-semibold" style="color: black;">
-                                <?= htmlspecialchars($sp['name']) ?>
-                            </h6>
+                <a href="index.php?supplier=<?= $sp['id'] ?>" class="text-decoration-none">
+                    <div class="supplier-card">
+                        <div class="supplier-image-wrapper">
+                            <img src="<?= htmlspecialchars($sp['image_url']) ?>"
+                                class="supplier-img"
+                                alt="<?= htmlspecialchars($sp['name']) ?>">
                         </div>
+                        <h6 class="supplier-name">
+                            <?= htmlspecialchars($sp['name']) ?>
+                        </h6>
                     </div>
                 </a>
-
             </div>
         <?php } ?>
     </div>
